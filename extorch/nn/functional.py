e61ba3547d35d039219d7ea9aa8a5491cd82e605
A-LinCui <zhaojunbo2012@sina.cn>
2021-12-02 17:14:32 +0800

    Fix bugs for mixup los

--- extorch/nn/functional.py
+++ extorch/nn/functional.py
@@ -8,28 +8,28 @@ import torch.nn as nn
 from extorch.utils import expect
 
 
-def mix_data(self, inputs: Tensor, targets: Tensor, alpha: float = 1.0):
+def mix_data(input: Tensor, target: Tensor, alpha: float = 1.0):
     r""" 
     Mixup data for training neural networks on convex combinations of pairs of examples and their labels (`Link`_).
 
     Args:
-        inputs (Tensor): Input examples.
-        targets (Tensor): Labels of input examples.
+        input (Tensor): Input examples.
+        target (Tensor): Labels of input examples.
         alpha (float): Parameter of the beta distribution. Default: 1.0.
     
     Returns:
-        mixed_inputs (Tensor): Input examples after mixup.
-        mixed_targets (Tensor): Labels of mixed inputs.
+        mixed_input (Tensor): Input examples after mixup.
+        mixed_target (Tensor): Labels of mixed inputs.
         _lambda (float): Parameter sampled from the beta distribution.
 
     .. _Link:
         https://arxiv.org/abs/1710.09412
     """
     _lambda = np.random.beta(alpha, alpha) if alpha > 0. else 1.
-    index = torch.randperm(len(targets)).to(inputs.device)
-    mixed_inputs = _lambda * inputs + (1 - _lambda) * inputs[index, :]
-    mixed_targets = targets[index]
-    return mixed_inputs, mixed_targets, _lambda
+    index = torch.randperm(len(target)).to(input.device)
+    mixed_input = _lambda * input + (1 - _lambda) * input[index, :]
+    mixed_target = target[index]
+    return mixed_input, mixed_target, _lambda
 
 
 def average_logits(logits_list: List[Tensor]) -> Tensor:

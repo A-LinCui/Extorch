5156d5e938772e9ff64e0625ac420236a273c282
A-LinCui <zhaojb17@mails.tsinghua.edu.cn>
2021-12-21 19:12:42 +0800

    Add net_device

--- extorch/nn/utils.py
+++ extorch/nn/utils.py
@@ -2,6 +2,7 @@ import six
 from contextlib import contextmanager
 from collections import OrderedDict
 
+import torch
 from torch import Tensor
 import torch.nn as nn
 
@@ -46,6 +47,31 @@ def use_params(module: nn.Module, params: OrderedDict) -> None:
     yield
     for mod_prefix, mod in module.named_modules():
         _substitute_params(mod, backup_params, prefix = mod_prefix)
+
+
+def net_device(module: nn.Module) -> torch.device:
+    r"""
+    Get current device of the network, assuming all weights of the network are on the same device.
+
+    Args:
+        module (nn.Module): The network.
+
+    Returns:
+        torch.device: The device.
+
+    Examples::
+      >>> module = nn.Conv2d(3, 3, 3)
+      >>> device = net_device(module) # "cpu"
+    """
+    if isinstance(module, nn.DataParallel):
+        module = module.module
+    for submodule in module.children():
+        parameters = submodule._parameters
+        if "weight" in parameters:
+            return parameters["weight"].device
+    parameters = module._parameters
+    assert "weight" in parameters
+    return parameters["weight"].device
 
 
 class WrapperModel(nn.Module):
